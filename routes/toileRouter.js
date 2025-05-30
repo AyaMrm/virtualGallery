1569c6e5e389d09bba4b7ace8b9e1a722ec96f91
MrmAya <kahlimeriemaya@gmail.com>
2025-05-30 22:49:13 +0200

    import oublier

--- routes/toileRouter.js
+++ routes/toileRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const toileController = require('../controllers/toileController');
+const {body, param, validationResult} = require('express-validator');
 
 
 router.post('/toiles', [
